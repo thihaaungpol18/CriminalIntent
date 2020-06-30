25b302ea4d9c37a68b5f1073f342ef0d9888bd4e
Thiha Eung <50638156+thihaaungpol18@users.noreply.github.com>
2020-06-30 12:27:11 +0630

    Optimized

--- app/src/main/java/com/thiha/criminalintent/view/CrimeFragment.kt
+++ app/src/main/java/com/thiha/criminalintent/view/CrimeFragment.kt
@@ -4,13 +4,14 @@ import android.app.Activity
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -53,42 +54,49 @@ class CrimeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(requireActivity()).get(ListViewModel::class.java)
+
         if (arguments != null) {
 
-            updateCrime()
+            currentPosition = arguments?.getInt(ARG_CRIME_ID)
 
-            viewModel.allCrimes.observe(viewLifecycleOwner, Observer { list ->
-                btn_send_report.setOnClickListener {
-                    var i = Intent(Intent.ACTION_SEND)
-                    val crime = list[currentPosition!!]
-                    i.type = "text/plain"
-                    i.putExtra(Intent.EXTRA_TEXT, getCrimeReport(crime))
-                    i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject))
-                    i = Intent.createChooser(i, getString(R.string.send_report_via))
-                    startActivity(i)
-                }
-            })
+            populateData()
+
+            btn_done.text = getString(R.string.update)
+
+            btn_done.setOnClickListener {
+                insertCrime()
+            }
+        } else {
+            btn_crime_date.setOnClickListener {
+                openPickers()
+            }
+
+
+            btn_done.text = getString(R.string.save)
+
+            btn_send_report.visibility = Button.GONE
+
+            btn_choose_suspect.setOnClickListener {
+                chooseSuspect()
+            }
 
             btn_done.setOnClickListener {
                 insertCrime()
             }
         }
-
-        btn_done.setOnClickListener {
-            insertCrime()
-        }
-
-        btn_choose_suspect.setOnClickListener {
-            chooseSuspect()
-        }
-
-        btn_crime_date.setOnClickListener {
-            openPickers()
-        }
     }
 
-    private fun updateCrime() {
-        currentPosition = arguments?.getInt(ARG_CRIME_ID)
+    private fun sendReport() {
+        var i = Intent(Intent.ACTION_SEND)
+        val crime = currentCrime!!
+        i.type = "text/plain"
+        i.putExtra(Intent.EXTRA_TEXT, getCrimeReport(crime))
+        i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject))
+        i = Intent.createChooser(i, getString(R.string.send_report_via))
+        startActivity(i)
+    }
+
+    private fun populateData() {
 
         viewModel.allCrimes.observe(viewLifecycleOwner, Observer {
             if (currentPosition != -1) {
@@ -98,30 +106,36 @@ class CrimeFragment : Fragment() {
 
                 et_title.setText(currentCrime!!.title)
 
-                val formatter = SimpleDateFormat("EEE, d MMM yyyy hh:mm aaa", Locale.US)
-                val date = formatter.format(currentCrime!!.date)
-                btn_crime_date.text = date
+                btn_crime_date.text = formatDate(currentCrime!!.date)
 
                 checkbox_crime_solved.isChecked = currentCrime!!.solved
+
                 checkbox_required_police.isChecked = currentCrime!!.requiredPolice
+
+                btn_choose_suspect.text = currentCrime!!.suspect
+
                 if (currentCrime!!.requiredPolice) {
                     btn_call_police.visibility = View.VISIBLE
                 }
 
                 btn_done.setOnClickListener {
-                    currentCrime!!.title = et_title.text.toString().trim()
-                    currentCrime!!.requiredPolice = checkbox_required_police.isChecked
-                    currentCrime!!.solved = checkbox_crime_solved.isChecked
-                    currentCrime!!.date = globalDate!!
-                    val result = viewModel.update(currentCrime!!)
-                    if (result.isCompleted || !result.isCancelled || result.isActive) {
-                        val snackBar = Snackbar.make(et_title, "Updated", Snackbar.LENGTH_LONG)
-                        snackBar.setAction("Dismiss") { snackBar.dismiss() }.show()
-                        findNavController().navigate(R.id.goHome)
-                    } else {
-                        Snackbar.make(et_title, "Cannot Update", Snackbar.LENGTH_LONG).show()
-                    }
+                    updateCrime()
                 }
+
+                btn_choose_suspect.setOnClickListener {
+                    chooseSuspect()
+                }
+
+                btn_send_report.setOnClickListener {
+                    sendReport()
+                }
+
+                btn_crime_date.setOnClickListener {
+                    openPickers()
+                }
+
+                btn_call_police.setOnClickListener { callPolice() }
+
             } else {
                 currentCrime = null
             }
@@ -129,49 +143,93 @@ class CrimeFragment : Fragment() {
     }
 
     private fun insertCrime() {
-        if (currentCrime != null) {
-            viewModel.update(currentCrime!!)
-        } else {
-            if (et_title.text.toString().trim().isNotEmpty()) {
-                val result = viewModel.insert(
-                    Crime(
-                        id = 0,
-                        title = et_title.text.toString().trim(),
-                        date = globalDate!!,
-                        solved = checkbox_crime_solved.isChecked,
-                        requiredPolice = (checkbox_required_police.isChecked)
-                        , suspect = btn_choose_suspect.text.toString()
+
+        if (btn_crime_date.text == getString(R.string.date)) {
+            val snackbar =
+                Snackbar.make(et_title, "Date Plz", Snackbar.LENGTH_SHORT)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                snackbar.setTextColor(
+                    resources.getColor(
+                        android.R.color.holo_red_dark,
+                        null
                     )
                 )
-                if (result.isActive || !result.isCancelled || result.isCompleted) {
-                    val snackbar = Snackbar.make(et_title, "Data Inserted", Snackbar.LENGTH_SHORT)
-                    snackbar.setAction("Dismiss") { snackbar.dismiss() }.show()
-                } else {
-                    val snackbar =
-                        Snackbar.make(et_title, "Data Inserted Fail", Snackbar.LENGTH_SHORT)
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        snackbar.setTextColor(
-                            resources.getColor(
-                                android.R.color.holo_red_dark,
-                                null
-                            )
-                        )
-                        snackbar.setActionTextColor(
-                            resources.getColor(
-                                android.R.color.holo_red_dark,
-                                null
-                            )
-                        )
-                        snackbar.setAction("Dismiss") { snackbar.dismiss() }.show()
-                    }
-                }
-            } else {
-                et_title.error = "Title Required"
-                et_title.requestFocus()
-                return
+                snackbar.setActionTextColor(
+                    resources.getColor(
+                        android.R.color.holo_red_dark,
+                        null
+                    )
+                )
+                snackbar.setAction(getString(R.string.setDate)) { openPickers() }.show()
             }
+            return
+        }
+
+        if (et_title.text.toString().trim().isNotEmpty()) {
+            val result = viewModel.insert(
+                Crime(
+                    id = 0,
+                    title = et_title.text.toString().trim(),
+                    date = globalDate!!,
+                    solved = checkbox_crime_solved.isChecked,
+                    requiredPolice = (checkbox_required_police.isChecked),
+                    suspect = btn_choose_suspect.text.toString()
+                )
+            )
+            if (result.isActive || !result.isCancelled || result.isCompleted) {
+                val snackbar =
+                    Snackbar.make(et_title, "Data Inserted", Snackbar.LENGTH_SHORT)
+                snackbar.setAction("Dismiss") { snackbar.dismiss() }.show()
+            } else {
+                val snackbar =
+                    Snackbar.make(et_title, "Data Inserted Fail", Snackbar.LENGTH_SHORT)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    snackbar.setTextColor(
+                        resources.getColor(
+                            android.R.color.holo_red_dark,
+                            null
+                        )
+                    )
+                    snackbar.setActionTextColor(
+                        resources.getColor(
+                            android.R.color.holo_red_dark,
+                            null
+                        )
+                    )
+                    snackbar.setAction("Dismiss") { snackbar.dismiss() }.show()
+                }
+            }
+        } else {
+            et_title.error = "Title Required"
+            et_title.requestFocus()
+            return
         }
         findNavController().navigate(R.id.goHome)
+    }
+
+    private fun updateCrime() {
+        if (et_title.text.toString().trim().isNotEmpty()) {
+            currentCrime!!.title = et_title.text.toString().trim()
+            currentCrime!!.requiredPolice = checkbox_required_police.isChecked
+            currentCrime!!.solved = checkbox_crime_solved.isChecked
+            currentCrime!!.date = globalDate!!
+            val result = viewModel.update(currentCrime!!)
+            if (result.isCompleted || !result.isCancelled || result.isActive) {
+                val snackBar = Snackbar.make(et_title, "Updated", Snackbar.LENGTH_SHORT)
+                snackBar.setAction("Dismiss") { snackBar.dismiss() }.show()
+                findNavController().navigate(R.id.goHome)
+            } else {
+                Snackbar.make(et_title, "Cannot Update", Snackbar.LENGTH_SHORT).show()
+            }
+        } else {
+            et_title.error = "Crime Title Plz"
+            et_title.requestFocus()
+        }
+    }
+
+    private fun formatDate(date: Date): String {
+        val formatter = SimpleDateFormat("EEE, d MMM yyyy hh:mm aaa", Locale.US)
+        return formatter.format(date)
     }
 
     private fun getCrimeReport(crime: Crime): String {
@@ -197,7 +255,6 @@ class CrimeFragment : Fragment() {
     }
 
     private fun chooseSuspect() {
-
         val contactIntent =
             Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
         startActivityForResult(contactIntent, CONTACT_REQUEST)
@@ -205,13 +262,14 @@ class CrimeFragment : Fragment() {
 
     private fun openPickers() {
 
+        val datePickerDialog: DatePickerDialog
+        val timePickerDialog: TimePickerDialog
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-
-        val timePickerDialog = TimePickerDialog(
+        timePickerDialog = TimePickerDialog(
             requireContext(),
             TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
                 globalHour = hourOfDay
@@ -225,31 +283,40 @@ class CrimeFragment : Fragment() {
                     globalMinute!!
                 ).time
 
-                val formatter = SimpleDateFormat("EEE, d MMM yyyy hh:mm aaa", Locale.US)
-                val formattedDate = formatter.format(date)
                 globalDate = date
-                btn_crime_date.text = formattedDate
+                btn_crime_date.text = formatDate(date)
+
             },
             c.get(Calendar.HOUR),
             c.get(Calendar.MINUTE),
             false
         )
 
-        val datePickerDialog = DatePickerDialog(
+        datePickerDialog = DatePickerDialog(
             requireContext(),
             DatePickerDialog.OnDateSetListener { _, setYear, monthOfYear, dayOfMonth ->
                 globalYear = setYear
                 globalMonth = monthOfYear
                 globalDay = dayOfMonth
 
-                timePickerDialog.show()
-
+                if (!timePickerDialog.isShowing) {
+                    timePickerDialog.show()
+                }
             },
             year,
             month,
             day
         )
-        datePickerDialog.show()
+
+
+        if (!datePickerDialog.isShowing) {
+            datePickerDialog.show()
+        }
+    }
+
+    private fun callPolice() {
+        val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: 199"))
+        startActivity(callIntent)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -265,16 +332,18 @@ class CrimeFragment : Fragment() {
                 if (cursor != null && cursor.count != 0) {
                     cursor.moveToFirst()
 
-                    val numberIndex =
-                        cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+//                    val numberIndex =
+//                        cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
 
                     val nameIndex =
                         cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
 
-                    val number = cursor.getString(numberIndex)
+//                    val number = cursor.getString(numberIndex)
                     val name = cursor.getString(nameIndex)
 
-                    Log.i("MyLog", " number : $number $name ")
+                    btn_choose_suspect?.text = name
+                    currentCrime?.suspect = name
+
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -286,7 +355,6 @@ class CrimeFragment : Fragment() {
     companion object {
         private const val ARG_CRIME_ID = "crime_id"
         private const val CONTACT_REQUEST = 3
-        private const val DATETIME_REQUEST = 2
         fun newInstance(crimeID: Int): Fragment {
             val bundle = Bundle()
             bundle.putInt(ARG_CRIME_ID, crimeID)
